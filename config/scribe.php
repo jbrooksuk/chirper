951e15a2b56d2caba1ab3d8ee857e5c0fd27894a
James Brooks <james@alt-three.com>
2023-01-19 12:10:17 +0000

    wip

--- config/scribe.php
+++ config/scribe.php
@@ -14,7 +14,7 @@ return [
     /*
      * A short description of your API. Will be included in the docs webpage, Postman collection and OpenAPI spec.
      */
-    'description' => 'This is a description of our API.',
+    'description' => '',
 
     /*
      * The base URL displayed in the docs. If this is empty, Scribe will use the value of config('app.url').
