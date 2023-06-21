219c06af30c435b79ed5061f1edd74c5245f9269
James Brooks <james@alt-three.com>
2023-06-21 11:05:42 +0100

    Disable try it out

--- config/scribe.php
+++ config/scribe.php
@@ -185,7 +185,7 @@ return [
          * Add a Try It Out button to your endpoints so consumers can test endpoints right from their browser.
          * Don't forget to enable CORS headers for your endpoints.
          */
-        'enabled' => true,
+        'enabled' => false,
 
         /**
          * The base URL for the API tester to use (for example, you can set this to your staging URL).
